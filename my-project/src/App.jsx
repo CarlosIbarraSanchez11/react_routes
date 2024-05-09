7ef714183ca5e1caaa27932c5e1b99137cef83ad
Carlos <carlosmanuelibarra11@gmail>
2024-05-09 00:18:56 -0500

    37:40

--- my-project/src/App.jsx
+++ my-project/src/App.jsx
@@ -1,35 +1,58 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
+import { Link, Route, Routes } from "react-router-dom";
 
-function App() {
-  const [count, setCount] = useState(0)
+// Componentes => Elementos
+const Home = () => <h1>Home</h1>;
+const SearchPage = () => {
+  const tacos = ["cochinita", "chili", "carnita", "quesadilla"];
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>Search Page</h1>
+      {/* <Link to={tacos}>Tacos</Link> */}
+      <ul>
+        {tacos.map((taco) => (
+          <li key={taco}>
+            {" "}
+            <Link to={`/tacos/${taco}`}>{taco}</Link>
+          </li>
+        ))}
+      </ul>
     </>
-  )
+  );
+};
+const Tacos = () => <h1>Tacos</h1>;
+
+function App() {
+  return (
+    <>
+      <header>
+        <h1>MichiShop</h1>
+        <nav>
+          <ul>
+            {/* <li>
+              <a href="/search-page">Search Page</a>
+            </li>
+            <li>
+              <a href="/">Home</a>
+            </li> */}
+            <li>
+              <Link to="/">Home</Link>
+              <Link to="/search-page">Search Page</Link>
+            </li>
+          </ul>
+        </nav>
+      </header>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/search-page" element={<SearchPage />} />
+        <Route path="/tacos/:name" element={<Tacos />} />
+      </Routes>
+    </>
+  );
 }
 
-export default App
+export default App;
+
+
+37:40 / 1:44:22
